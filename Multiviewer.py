2b1b8847b3686210143684dd0e3efb53c39e911a
hyena Park(Linux) <ponmoa@naver.com>
2023-01-31 18:41:57 +0900

    Add interface : detected points test. and Refactoring whole code...

--- Multiviewer.py
+++ Multiviewer.py
@@ -4,9 +4,6 @@ import os
 import json
 import natsort
 
-# print(cv2.__version__)
-# print(os.getcwd())
-
 src = []
 src_raw = []
 src_w = 1920
@@ -14,23 +11,106 @@ src_h = 1080
 cam_pts = []
 world_pts = []
 
+
+### json import (points)
+json_file = open('/home/hnpark/data/1.pts')
+json_data = json.load(json_file)
+src_points = [[0 for j in range(5)] for i in range(len(json_data['points']))]
+def importJson():
+    for i in range(len(json_data['points'])):
+    # src_points = [[0 for j in range(5)] for i in range(src_num)]
+    # for i in range(src_num):
+        src_points[i][0] = json_data['points'][i]['dsc_id']
+        x1 = json_data['points'][i]['pts_3d']['X1']
+        x2 = json_data['points'][i]['pts_3d']['X2']
+        x3 = json_data['points'][i]['pts_3d']['X3']
+        x4 = json_data['points'][i]['pts_3d']['X4']
+        y1 = json_data['points'][i]['pts_3d']['Y1']
+        y2 = json_data['points'][i]['pts_3d']['Y2']
+        y3 = json_data['points'][i]['pts_3d']['Y3']
+        y4 = json_data['points'][i]['pts_3d']['Y4']
+        src_points[i][1] = (x1, y1)
+        src_points[i][2] = (x2, y2)
+        src_points[i][3] = (x3, y3)
+        src_points[i][4] = (x4, y4)
+    world_pts.append(json_data['stadium'])
+    wx1 = json_data['world_coords']['X1']
+    wx2 = json_data['world_coords']['X2']
+    wx3 = json_data['world_coords']['X3']
+    wx4 = json_data['world_coords']['X4']
+    wy1 = json_data['world_coords']['Y1']
+    wy2 = json_data['world_coords']['Y2']
+    wy3 = json_data['world_coords']['Y3']
+    wy4 = json_data['world_coords']['Y4']
+    world_pts.append((654,510))
+    world_pts.append((654,296))
+    world_pts.append((697,296))
+    world_pts.append((697,510))
+
+importJson()
+
 class Point:
     x = 0.0
     y = 0.0
 
+### calc warp point
+dst_pts = []
+world_points = list()
+for i in range(1,5):
+    p = Point()
+    p.x = world_pts[i][0]
+    p.y = world_pts[i][1]
+    world_points.append(p)
+dst_pts = np.array([[e.x,e.y] for e in world_points])
+def findHomo(cam_idx):
+    points = list()
+    for i in range(1,5):
+        p = Point()
+        p.x = src_points[cam_idx][i][0]
+        p.y = src_points[cam_idx][i][1]
+        points.append(p)
+    src_pts = np.array([[e.x,e.y] for e in points])
+    # print(src_pts)
+    H, status = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC)
+    # print("homo matrix: ", H)
+    return H
 
-# src import
+def findHomo_wtos(cam_idx):
+    points = list()
+    for i in range(1,5):
+        p = Point()
+        p.x = src_points[cam_idx][i][0]
+        p.y = src_points[cam_idx][i][1]
+        points.append(p)
+    src_pts = np.array([[e.x,e.y] for e in points])
+    # print(src_pts)
+    H, status = cv2.findHomography(dst_pts, src_pts, cv2.RANSAC)
+    # print("homo matrix: ", H)
+    return H
+
+def calcPointHomo(H, pts):
+    x = (H[0][0] * pts.x + H[0][1] * pts.y + H[0][2]) / (H[2][0]*pts.x + H[2][1]*pts.y + 1)
+    y = (H[1][0] * pts.x + H[1][1] * pts.y + H[1][2]) / (H[2][0]*pts.x + H[2][1]*pts.y + 1)
+    return (int(x),int(y))
+
+### src img import
 src_path = '/home/hnpark/data/1/'
 src_list = os.listdir(src_path)
 src_list = natsort.natsorted(src_list)
 src_num = len(src_list)
 
 
-# world img import
-world_img = cv2.imread('/home/hnpark/data/IceLink.png')
+### save each homo matrix
+h_sw = [[0 for j in range(9)] for i in range(src_num)]
+for i in range(src_num):
+    h_sw[i] = findHomo(i)
+
+h_ws = [[0 for j in range(9)] for i in range(src_num)]
+for i in range(src_num):
+    h_ws[i] = findHomo_wtos(i)
 
 
-# calc viewer row & resize rate
+### calc viewer row & resize rate
 multiview_row = 1
 while 1:
     multiview_row += 1
@@ -38,64 +118,57 @@ while 1:
         break
 resize_rate = 1 / multiview_row
 
+### world img import
+world_img = cv2.imread('/home/hnpark/data/IceLink.png')
 
-# json import (points)
-json_file = open('/home/hnpark/data/1.pts')
-json_data = json.load(json_file)
-src_points = [[0 for j in range(5)] for i in range(src_num)]
-for i in range(src_num):
-    src_points[i][0] = json_data['points'][i]['dsc_id']
-    x1 = json_data['points'][i]['pts_3d']['X1']
-    x2 = json_data['points'][i]['pts_3d']['X2']
-    x3 = json_data['points'][i]['pts_3d']['X3']
-    x4 = json_data['points'][i]['pts_3d']['X4']
-    y1 = json_data['points'][i]['pts_3d']['Y1']
-    y2 = json_data['points'][i]['pts_3d']['Y2']
-    y3 = json_data['points'][i]['pts_3d']['Y3']
-    y4 = json_data['points'][i]['pts_3d']['Y4']
-    src_points[i][1] = (x1, y1)
-    src_points[i][2] = (x2, y2)
-    src_points[i][3] = (x3, y3)
-    src_points[i][4] = (x4, y4)
-world_pts.append(json_data['stadium'])
-wx1 = json_data['world_coords']['X1']
-wx2 = json_data['world_coords']['X2']
-wx3 = json_data['world_coords']['X3']
-wx4 = json_data['world_coords']['X4']
-wy1 = json_data['world_coords']['Y1']
-wy2 = json_data['world_coords']['Y2']
-wy3 = json_data['world_coords']['Y3']
-wy4 = json_data['world_coords']['Y4']
-world_pts.append((654,510))
-world_pts.append((654,296))
-world_pts.append((697,296))
-world_pts.append((697,510))
-
-
-# draw world pts
+### draw world pts
 world_view = cv2.circle(world_img, world_pts[1], 4, (0, 0, 255), 2)
 world_view = cv2.circle(world_img, world_pts[2], 4, (0, 255, 255), 2)
 world_view = cv2.circle(world_img, world_pts[3], 4, (0, 255, 0), 2)
 world_view = cv2.circle(world_img, world_pts[4], 4, (255, 0, 0), 2)
 
 
-# save images & resize
+
+
+### detected pts
+detected_pts = [[0,[(1000,500),(400,600),(500,600)]],[1,[(1200,760),(1300,600)]]]
+detected_warp = detected_pts[:]
+
+### save images & resize
 for i in src_list:
     index = src_list.index(i)
     img = cv2.imread(src_path + i)
+    img = cv2.circle(img, (int(src_points[index][1][0]), int(src_points[index][1][1])), 5, (0, 0, 255), -1)
+    img = cv2.circle(img, (int(src_points[index][2][0]), int(src_points[index][2][1])), 5, (0, 255, 255), -1)
+    img = cv2.circle(img, (int(src_points[index][3][0]), int(src_points[index][3][1])), 5, (0, 255, 0), -1)
+    img = cv2.circle(img, (int(src_points[index][4][0]), int(src_points[index][4][1])), 5, (255, 0, 0), -1)
+    ### save warp p
+    for pts in detected_pts:
+        if index in pts:
+            for p in detected_pts[index][1]:
+                pdx = detected_pts[index][1].index(p)
+                cv2.circle(img, p, 10, (255,0,255), 3)
+                _p = Point()
+                _p.x = p[0]
+                _p.y = p[1]
+                warp_p = calcPointHomo(h_sw[index], _p)
+                detected_warp[index][1][pdx] = warp_p
+        else:
+            pass
+
     src_raw.append(img)
     img_rsz = cv2.resize(img, (0, 0), fx=resize_rate, fy=resize_rate, interpolation=cv2.INTER_AREA)
     cv2.putText(img_rsz, str(index), (10,25), cv2.FONT_HERSHEY_DUPLEX, 1, (255,255,255) )
-
-    # draw pts
-    img_pts = cv2.circle(img_rsz, (int(src_points[index][1][0] * resize_rate), int(src_points[index][1][1])), 2, (0, 0, 255), -1)
-    img_pts = cv2.circle(img_rsz, (int(src_points[index][2][0] * resize_rate), int(src_points[index][2][1])), 2, (0, 255, 255), -1)
-    img_pts = cv2.circle(img_rsz, (int(src_points[index][3][0] * resize_rate), int(src_points[index][3][1])), 2, (0, 255, 0), -1)
-    img_pts = cv2.circle(img_rsz, (int(src_points[index][4][0] * resize_rate), int(src_points[index][4][1])), 2, (255, 0, 0), -1)
-    src.append(img_pts)
+    src.append(img_rsz)
 
 
-# tile to multiview
+### draw detected pts
+for l in detected_warp:
+    for p in l[1]:
+        print(p)
+        cv2.circle(world_view, p, 3, (50,210,240), -1)
+
+### tile to multiview
 def concat_tile(im_list_2d):
     return cv2.vconcat([cv2.hconcat(im_list_h) for im_list_h in im_list_2d])
 
@@ -115,13 +188,13 @@ makeMultiView()
 multi_view = concat_tile(single_view)
 
 
-# show windows
+### show windows
 key_command = " Single Main View " \
               ": Press 'cam number' + 'Enter'. "
 cv2.putText(multi_view, key_command, (100,multi_view.shape[0]-200), cv2.FONT_HERSHEY_DUPLEX, 1, (0,255,0))
 cv2.imshow("multi view", multi_view)
 cv2.imshow("world view", world_view)
-#cv2.waitKey(0)
+
 
 single_index = 0
 main_single_view = src_raw[single_index]
@@ -133,7 +206,7 @@ cur_point = Point()
 warp_point = Point()
 cntClick = 0
 
-# mouse event
+### mouse event
 def onMouse(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         cur_point.x = x
@@ -181,7 +254,7 @@ def drawWarpPoint(cur_point):
     cv2.imshow("main view", main_single_view)
     cv2.imshow("world view", world_view)
 
-    # to multiview
+    ### to multiview
     wp = Point()
     wp.x = warp_p[0]
     wp.y = warp_p[1]
@@ -196,59 +269,7 @@ def drawWarpPoint(cur_point):
     cv2.imshow("multi view", multi_view)
 
 
-# calc warp point
-dst_pts = []
-world_points = list()
-for i in range(1,5):
-    p = Point()
-    p.x = world_pts[i][0]
-    p.y = world_pts[i][1]
-    world_points.append(p)
-dst_pts = np.array([[e.x,e.y] for e in world_points])
-# print("dst pts: ", dst_pts)
-
-
-def findHomo(cam_idx):
-    points = list()
-    for i in range(1,5):
-        p = Point()
-        p.x = src_points[cam_idx][i][0]
-        p.y = src_points[cam_idx][i][1]
-        points.append(p)
-    src_pts = np.array([[e.x,e.y] for e in points])
-    # print(src_pts)
-    H, status = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC)
-    # print("homo matrix: ", H)
-    return H
-
-def findHomo_wtos(cam_idx):
-    points = list()
-    for i in range(1,5):
-        p = Point()
-        p.x = src_points[cam_idx][i][0]
-        p.y = src_points[cam_idx][i][1]
-        points.append(p)
-    src_pts = np.array([[e.x,e.y] for e in points])
-    # print(src_pts)
-    H, status = cv2.findHomography(dst_pts, src_pts, cv2.RANSAC)
-    # print("homo matrix: ", H)
-    return H
-
-def calcPointHomo(H, pts):
-    x = (H[0][0] * pts.x + H[0][1] * pts.y + H[0][2]) / (H[2][0]*pts.x + H[2][1]*pts.y + 1)
-    y = (H[1][0] * pts.x + H[1][1] * pts.y + H[1][2]) / (H[2][0]*pts.x + H[2][1]*pts.y + 1)
-    return (int(x),int(y))
-
-h_sw = [[0 for j in range(9)] for i in range(src_num)]
-for i in range(src_num):
-    h_sw[i] = findHomo(i)
-
-h_ws = [[0 for j in range(9)] for i in range(src_num)]
-for i in range(src_num):
-    h_ws[i] = findHomo_wtos(i)
-
-
-# 3d world view
+### 3d world view
 cv2.namedWindow("3d world view")
 c_h = 4
 c_d = 3
@@ -262,7 +283,7 @@ cv2.setTrackbarPos("dy", "3d world view", 300)
 
 world_rsz = cv2.resize(world_view,(1600,1600))
 world_cut = world_rsz[400:world_l, 0:1600].copy()
-#after cutting
+### after cutting
 wp1 = [0,0]
 wp2 = [world_cut.shape[1], wp1[1]]
 wp3 = [wp1[0], world_cut.shape[0]]
@@ -282,7 +303,7 @@ def view3Dworld():
     return mtrx, dx, dy
 
 
-# key event
+### key event
 while True:
     m, _x, _y = view3Dworld()
     world_rsz = cv2.resize(world_view, (1600, 1600))
@@ -290,7 +311,6 @@ while True:
     world_3d = cv2.warpPerspective(world_cut, m, (1600,800))
     world_3d_cut = world_3d[_y:wp3[1],0:1600].copy()
     cv2.imshow("3d world view", world_3d_cut)
-
 
     key = cv2.waitKey(0)
     if key &0xFF == 27:
